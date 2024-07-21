e963859f2f64f0072e0f2cc7a8f6222388dbdfa8
Nyoman Adiwinanda <nyoman.adi16@gmail.com>
2024-07-22 00:51:10 +0700

    deploy to firebase

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./pages/Auth";
 import ProductList from "./pages/ProductList";
 import ProductDetail from "./pages/ProductDetail";
+import ScrollToTop from "./utils/ScrollToTop";
 import "./App.css";
 
 function App() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Auth />} />
-				<Route path="/product" element={<ProductList />} />
-				<Route path="/product/:id" element={<ProductDetail />} />
-			</Routes>
+			<ScrollToTop>
+				<Routes>
+					<Route path="/" element={<Auth />} />
+					<Route path="/product" element={<ProductList />} />
+					<Route path="/product/:id" element={<ProductDetail />} />
+				</Routes>
+			</ScrollToTop>
 		</BrowserRouter>
 	);
 }
